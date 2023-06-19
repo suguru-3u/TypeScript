4d37567081c97d538b27caadcdf0e9d77f7b935d
suguru-3u <suguru.13579515@gmail.com>
2023-06-20 07:21:39 +0900

    study ts

--- class&interface/class.ts
+++ class&interface/class.ts
@@ -3,6 +3,14 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  // thisは何を参照するのか、引数にthisを使って指定することができる
+  // Department型を参照するように指定している
+  describe(this: Department) {
+    // thisは呼び出し元を参照する。
+    console.log("Department: ", this.name);
+  }
 }
 
 const accounting = new Department("Accounting");
+accounting.describe();
