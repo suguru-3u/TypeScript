7ebce0a7c9f7a432271385db6e78b56fb6931431
suguru-3u <suguru.13579515@gmail.com>
2023-06-21 07:22:22 +0900

    study class private public

--- class&interface/class.ts
+++ class&interface/class.ts
@@ -1,5 +1,10 @@
 class Department {
-  private name: string;
+  // デフォルトはパブリック。publicの指定は不要(class外からアクセス可能)
+  public name: string;
+
+  // class内でしかアクセスできなくなる
+  private employes: string[] = [];
+
   constructor(name: string) {
     this.name = name;
   }
@@ -10,7 +15,22 @@ class Department {
     // thisは呼び出し元を参照する。
     console.log("Department: ", this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employes.push(employee);
+  }
+
+  printEmplyoleeInfo() {
+    console.log(this.employes);
+  }
 }
 
 const accounting = new Department("Accounting");
 accounting.describe();
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Mamu");
+
+accounting.printEmplyoleeInfo(); // Max,Mamus
+
+// accounting.employes[2] = "taro";
